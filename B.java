550460af95fa2b2d5e50462d55654de90c24c6ce
Amit <147873683+AmitKumarRawat@users.noreply.github.com>
2024-03-02 18:39:02 +0530

    Update InterfaceExample.java

--- B.java
+++ B.java
@@ -1,8 +1,8 @@
-interface Test1
+interface Test1     // definng concept of interfave in java
 {
  void dis();
 }
-class A implement Test1
+class A implement Test1   // implenting a class 
 {
 void dis()
 {
@@ -11,6 +11,6 @@ System.out.println(" Testing interface property of abstraction : ");
 public static void main(String args[])
 {
 A obj = new A();
-obj.dis();
+obj.dis(); // calling  through object of an interface 
 }
 }
