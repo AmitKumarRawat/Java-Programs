cf9a2fd6e4aae78cf9903e50ed4dea15bc01797d
Amit <147873683+AmitKumarRawat@users.noreply.github.com>
2024-03-02 18:41:54 +0530

    Update Exa.java

--- Exa.java
+++ Exa.java
@@ -1,9 +1,10 @@
+                // java program for graphics & defing applet class 
 import java.applet.Applet;
 import java.awt.Graphics;
-public class Exa extends Applet
+public class Exa extends Applet  // inherting Applet class here from applet package
 {
 public void paint(Graphics g)
 {
-g.drawString("Hello everyone ",150,160);
+g.drawString("Hello everyone ",150,160); 
 }
 }
