2398cd097474cc3fd571962402e7d8876253379f
Amit <147873683+AmitKumarRawat@users.noreply.github.com>
2024-03-02 18:34:58 +0530

    Update Amitpa.java
    This is a logic based program.

--- Amitpa.java
+++ Amitpa.java
@@ -1,11 +1,11 @@
-class Amitpa
+class Amitpa           //  PROGRAM TO FIND PALINDROME NUMBER IN JAVA
 {
 void palindrome(int i)
 {
 int c=0,s=0,r=0;
 c=i;
 while(i!=0)
-{
+{               // CERATING LOGIC
 s=i%10;    //s=1 ,s=2,s=1
 r=(r*10)+s;//r=1,12, 121
 i=i/10;// i=12,1
